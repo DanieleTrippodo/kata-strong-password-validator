2aa24791b4be07ff257f4b807fe6f67fe3c68d06
DanieleTrippodo <trippododaniele@gmail.com>
2024-10-20 18:39:28 +0200

    complete logic on js

--- script.js
+++ script.js
@@ -20,4 +20,51 @@ passwordInput.addEventListener('input', function() {
     const uppercaseValid = checkUppercase(password);
     const numberValid = checkNumber(password);
     const specialValid = checkSpecialChar(password);
+
+
+
+    // Aggiorniamo la barra di robustezza
+    updateStrengthBar([lengthValid, uppercaseValid, numberValid, specialValid]);
+
+    // Aggiorniamo la lista dei requisiti
+    updateRequirement(requirements.length, lengthValid);
+    updateRequirement(requirements.uppercase, uppercaseValid);
+    updateRequirement(requirements.number, numberValid);
+    updateRequirement(requirements.special, specialValid);
 });
+
+
+
+// Sezione Funzioni --------------------------------------------------------
+// Funzioni di validazione
+function checkLength(password) {
+  return password.length >= 9;
+}
+
+function checkUppercase(password) {
+  return /[A-Z]/.test(password);
+}
+
+function checkNumber(password) {
+  return /[0-9]/.test(password);
+}
+
+function checkSpecialChar(password) {
+  return /[!@#$%^&*(),.?":{}|<>]/.test(password);
+}
+
+// Funzione per aggiornare la barra di robustezza
+function updateStrengthBar(validations) {
+  const validCount = validations.filter(Boolean).length;
+  const strengthPercent = (validCount / validations.length) * 100;
+  strengthBar.style.width = strengthPercent + '%';
+}
+
+// Funzione per aggiornare i requisiti
+function updateRequirement(element, isValid) {
+  if (isValid) {
+    element.classList.add('valid');
+  } else {
+    element.classList.remove('valid');
+  }
+}
